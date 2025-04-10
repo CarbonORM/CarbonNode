a2221365ebbf6b84eb7a74d81c8c7b16634d91db
Richard T. Miles <Richard@miles.systems>
2025-04-10 11:25:18 -0600

    working to get a cross compat package

--- src/variables/isTest.ts
+++ src/variables/isTest.ts
@@ -1,4 +1,19 @@
 
-const isTest = process.env.JEST_WORKER_ID !== undefined || process.env.NODE_ENV === 'test'
+const isTest =
+    typeof process !== 'undefined' &&
+    typeof process.env !== 'undefined' &&
+    (
+        process.env.JEST_WORKER_ID !== undefined ||
+        process.env.NODE_ENV === 'test' ||
+        process.env.REACT_APP_TEST === 'true'
+    ) ||
+    // Vite-only branch
+    typeof import.meta !== 'undefined' &&
+    (
+        // @ts-ignore
+        import.meta?.env?.MODE === 'test' ||
+        // @ts-ignore
+        import.meta?.env?.VITE_TEST === 'true'
+    );
 
 export default isTest;
