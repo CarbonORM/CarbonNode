0ff2bf57eeb395a5b3ae2d9421d939e550ef7755
Richard T. Miles <Richard@miles.systems>
2025-07-16 20:32:50 -0600

    fix typescript infers

--- src/api/restOrm.ts
+++ src/api/restOrm.ts
@@ -1,6 +1,12 @@
 import restRequest from "./restRequest";
-import {OrmGenerics} from "./types/ormGenerics";
-import {iRest, iRestMethods} from "./types/ormInterfaces";
+import { OrmGenerics } from "./types/ormGenerics";
+import { iC6RestfulModel, iRest, iRestMethods } from "./types/ormInterfaces";
+
+type iCallRest = "Get" | "Put" | "Post" | "Delete";
+
+type RestOrmReturnType<G extends Omit<OrmGenerics, "requestMethod">> = {
+    [key in iCallRest]: ReturnType<typeof restRequest<G>>;
+} & iC6RestfulModel<G['RestShortTableName'], G['RestTableInterface'], G['PrimaryKey']>;
 
 export function restOrm<
     G extends Omit<OrmGenerics, "requestMethod">
@@ -8,23 +14,22 @@ export function restOrm<
     configFn: () => (
         Omit<iRest<G['RestShortTableName'], G['RestTableInterface'], G['PrimaryKey']>, "requestMethod">
         )
-) {
+): RestOrmReturnType<G> {
 
     const methods: iRestMethods[] = ["GET", "PUT", "POST", "DELETE"];
 
-    // this is so runtime variables can be used in the restOrm function and not just constants
     const userConfig = configFn();
+
+    const restMethods = methods.reduce((acc, method) => ({
+        ...acc,
+        [method[0] + method.slice(1).toLowerCase()]: restRequest<G>(() => ({
+            ...userConfig,
+            requestMethod: method as iRestMethods,
+        }))
+    }), {} as { [key in iCallRest]: ReturnType<typeof restRequest<G>> });
 
     return {
         ...userConfig.restModel,
-        ...Object.fromEntries(
-            methods.map(method => [
-                method[0] + method.slice(1).toLowerCase(), // Capitalize e.g. "Get"
-                restRequest<G>(() => ({
-                    ...userConfig,
-                    requestMethod: method as iRestMethods,
-                }))
-            ])
-        )
-    }
+        ...restMethods,
+    };
 }
